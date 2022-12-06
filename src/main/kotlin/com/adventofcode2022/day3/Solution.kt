6b68169cec62fc619f915b0548ffad7e4bc54f00
Emil Mattsson <emil.mattsson@resurs.se>
2022-12-06 15:08:08 +0100

    Solve day 3 part 2

--- src/main/kotlin/com/adventofcode2022/day3/Solution.kt
+++ src/main/kotlin/com/adventofcode2022/day3/Solution.kt
@@ -3,26 +3,39 @@ package com.adventofcode2022.day3
 import com.adventofcode2022.util.RESOURCES_BASE_PATH
 import java.io.File
 
-val bigCharPointMap = getCharPoints('A', 'Z', 27)
-val charPointMap = getCharPoints('a', 'z', 1)
+val bigAlphabetPointMap = getCharPoints('A', 'Z', 27)
+val alphabetPointMap = getCharPoints('a', 'z', 1)
 
 fun solution () {
-    charPointMap.putAll(bigCharPointMap)
+    alphabetPointMap.putAll(bigAlphabetPointMap)
 
     val sum = File("${RESOURCES_BASE_PATH}/day_3.txt").useLines { it.toList() }
-        .map { line ->
-            val lineMiddleIndex = line.length - (line.length / 2)
-            val firstCompartment = line.substring(0, lineMiddleIndex)
-            val secondCompartment = line.substring(lineMiddleIndex)
-
-            charPointMap[getCharFromBothCompartments(Rucksack(firstCompartment, secondCompartment))[0]]
+        .chunked(3)
+        .map { lines ->
+            getCharPointFrom(lines)
         }
         .sumOf { i -> i ?: 0 }
 
     println(sum)
 }
 
-private fun getCharFromBothCompartments(r: Rucksack) =
+private fun getCharPointFrom(lines: List<String>): Int? {
+    val chars1 = lines[0].toCharArray().distinct()
+    val chars2 = lines[1].toCharArray().distinct()
+    val chars3 = lines[2].toCharArray().distinct()
+
+    return alphabetPointMap[chars1.filter { char -> chars2.contains(char) && chars3.contains(char) }[0]]
+}
+
+private fun getCharPoint(line: String): Int? {
+    val lineMiddleIndex = line.length - (line.length / 2)
+    val firstCompartment = line.substring(0, lineMiddleIndex)
+    val secondCompartment = line.substring(lineMiddleIndex)
+
+    return alphabetPointMap[getCommonCharFromCompartments(Rucksack(firstCompartment, secondCompartment))[0]]
+}
+
+private fun getCommonCharFromCompartments(r: Rucksack) =
     r.secondCompartment.toCharArray().distinct().filter { char -> r.firstCompartment.toCharArray().contains(char) }
 
 fun getCharPoints(startChar: Char, endChar: Char, startValue: Int): MutableMap<Char, Int> {
